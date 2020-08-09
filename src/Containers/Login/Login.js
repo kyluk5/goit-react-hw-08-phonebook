3d430c9022a9c44fb104bafd82099bd0c1c06cec
Andrii Kulyk <kyluk5@gmail.com>
2020-08-09 23:09:06 +0300

    resive value from inputs

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { navigation } from "../navigation";
 import { NavLink } from "react-router-dom";
 // import PropTypes from 'prop-types';
 import "./Login.css";
 
-const Login = (props) => (
-  <>
-    <h1 className="registration-title">Log In</h1>
-    <form className="registration-form">
-      <input type="email" placeholder="email" className="reginstration-input" />
-      <input
-        type="password"
-        placeholder="password"
-        className="reginstration-input"
-      />
-    </form>
-    <button className="registaration-button">Registration</button>
-    <p className="log-in">
-      If you dont have account, please folow{" "}
-      <NavLink to={navigation.registration}>Registration</NavLink>
-    </p>
-  </>
-);
+const initialState = { email: "", password: "" };
+
+const Login = () => {
+  const [form, setForm] = useState(initialState);
+  const inputHandler = ({ target }) => {
+    const { name, value } = target;
+    setForm((state) => ({
+      ...state,
+      [name]: value,
+    }));
+    // console.log(form);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <>
+      <h1 className="log-in-title">Log In</h1>
+      <form className="log-in-form" onSubmit={submitForm}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="log-in-input"
+          value={form.email}
+          onChange={inputHandler}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="log-in-input"
+          value={form.password}
+          onChange={inputHandler}
+        />
+      </form>
+      <button className="log-in-button">Sign In</button>
+      <p className="log-in">
+        If you dont have account, please folow{" "}
+        <NavLink to={navigation.registration}>Registration</NavLink>
+      </p>
+    </>
+  );
+};
 
 Login.propTypes = {
   // bla: PropTypes.string,
--- src/Containers/Registration/Registration.js
+++ src/Containers/Registration/Registration.js
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { navigation } from "../navigation";
+import { NavLink } from "react-router-dom";
 // import PropTypes from 'prop-types';
 import "./Registration.css";
-import { NavLink } from "react-router-dom";
 
-const Registration = (props) => (
-  <>
-    <h1 className="registration-title">Registration</h1>
-    <form className="registration-form">
-      <input type="name" placeholder="name" className="reginstration-input" />
-      <input type="email" placeholder="email" className="reginstration-input" />
-      <input
-        type="password"
-        placeholder="password"
-        className="reginstration-input"
-      />
-    </form>
-    <button className="registaration-button">Registration</button>
-    <p className="log-in">
-      If you alredy have account, please{" "}
-      <NavLink to={navigation.login}>Log In</NavLink>
-    </p>
-  </>
-);
+const initialState = { name: "", email: "", password: "" };
+
+const Registration = () => {
+  const [form, setForm] = useState(initialState);
+  const inputHandler = ({ target }) => {
+    const { name, value } = target;
+    setForm((state) => ({
+      ...state,
+      [name]: value,
+    }));
+    // console.log(form);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <>
+      <h1 className="registration-title">Registration</h1>
+      <form className="registration-form" onSubmit={submitForm}>
+        <input
+          type="name"
+          name="name"
+          placeholder="name"
+          className="reginstration-input"
+          onChange={inputHandler}
+          value={form.name}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="reginstration-input"
+          onChange={inputHandler}
+          value={form.email}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="reginstration-input"
+          onChange={inputHandler}
+          value={form.password}
+        />
+      </form>
+      <button className="registaration-button">Registration</button>
+      <p className="log-in">
+        If you alredy have account, please{" "}
+        <NavLink to={navigation.login}>Log In</NavLink>
+      </p>
+    </>
+  );
+};
 
 Registration.propTypes = {
   // bla: PropTypes.string,
