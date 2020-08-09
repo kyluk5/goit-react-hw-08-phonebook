30f84708d22d9d7d91d0da2b38ca7bfc7de898bb
Andrii Kulyk <kyluk5@gmail.com>
2020-08-09 21:31:30 +0300

    login page

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { navigation } from "../navigation";
+import { NavLink } from "react-router-dom";
 // import PropTypes from 'prop-types';
-//import { Test } from './Login.styles';
+import "./Login.css";
 
-const Login = (props) => <div className="LoginWrapper">Test content</div>;
+const Login = (props) => (
+  <>
+    <h1 className="registration-title">Log In</h1>
+    <form className="registration-form">
+      <input type="email" placeholder="email" className="reginstration-input" />
+      <input
+        type="password"
+        placeholder="password"
+        className="reginstration-input"
+      />
+    </form>
+    <button className="registaration-button">Registration</button>
+    <p className="log-in">
+      If you dont have account, please folow{" "}
+      <NavLink to={navigation.registration}>Registration</NavLink>
+    </p>
+  </>
+);
 
 Login.propTypes = {
   // bla: PropTypes.string,
