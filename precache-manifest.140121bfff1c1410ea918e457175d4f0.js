624762006cfc6b3f14ed79766e7f18ae67f840fd
Andrii Kulyk <kyluk5@gmail.com>
2020-08-11 19:45:10 +0300

    Updates

---
+++ precache-manifest.140121bfff1c1410ea918e457175d4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbed6a603da2bb480efaab9fec51c2cf",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "afd1e07ba7efe75c6a14",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.8d3f3717.chunk.css"
+  },
+  {
+    "revision": "2cb0be71e2f8aef56576",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.b1e550f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.b1e550f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd1e07ba7efe75c6a14",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.53f59223.chunk.js"
+  },
+  {
+    "revision": "2a13091ca041de263bee",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.650e6e2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-08-phonebook/precache-manifest.e621cba41a209d6964c42f95a76672b0.js"
+  "/goit-react-hw-08-phonebook/precache-manifest.140121bfff1c1410ea918e457175d4f0.js"
 );
 
 self.addEventListener('message', (event) => {
